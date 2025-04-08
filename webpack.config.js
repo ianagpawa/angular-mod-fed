bb91e0cea764b196edeb1b3971c63e2df6d87135
Ian Agpawa <agpawaji@gmail.com>
2025-04-08 00:50:01 -0400

    feat: add mod fed config as remote module

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,47 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
 
-module.exports = withModuleFederationPlugin({
-    name: 'ang-remote',
-    exposes: {
-        './Module': './src/app/export/export.module.ts'
-    },
-    shared: {
-        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-    },
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, 'tsconfig.json'),
+  [/* mapped paths to share */]);
 
-});
+module.exports = {
+  output: {
+    uniqueName: "angRemote",
+    publicPath: "auto",
+    scriptType:'text/javascript'
+  },
+  optimization: {
+    runtimeChunk: false
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        // For remotes (please adjust)
+        name: "angRemote",
+        filename: "remoteEntry.js",
+        exposes: {
+            './Module':'./src/remote-bootstrap.ts'
+        },
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
